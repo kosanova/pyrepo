88dfa6ba4b5e374e2eb5309862ed5eda8224d211
emickos <michal.kosinski@ericsson.com>
2016-09-29 16:49:31 +0200

    Added argparse and options

--- pace_counter.py
+++ pace_counter.py
@@ -2,14 +2,24 @@
 
 #add options like halfmarathon, marathon, negativesplit - use a proper library
 
-import sys
-#import argparse
+import argparse
 
-script_name, distance, time = sys.argv
+parser = argparse.ArgumentParser()
 
-#parser = argparse.ArgumentParser()
-#parser.parse_args()
+parser.add_argument("-d", "--distance", help = "The distance to run", type = float)
+parser.add_argument("time", help = "Goal time")
+parser.add_argument("-m", "--marathon", help = "The marathon distance", action = "store_true")
+parser.add_argument("-hm", "--halfmarathon", help = "The half-marathon distance", action = "store_true")
+args = parser.parse_args()
 
+if args.marathon:
+    distance = 42.195
+elif args.halfmarathon:
+    distance = 21.0975
+else:
+    distance = args.distance
+    
+time = args.time
 
 #returns time in seconds
 def parseTime(str_time):
@@ -19,15 +29,13 @@ def parseTime(str_time):
     hours = str_time[:delimiter_left_idx]
     minutes = str_time[delimiter_left_idx+1:delimiter_right_idx]
     seconds = str_time[delimiter_right_idx+1:]
-    
-    #print delimiter_right_idx, delimiter_left_idx 
-    #print hours, minutes, seconds
+
     return int(hours)*60*60 + int(minutes)*60 + int(seconds)
-  
-#return pace time in seconds  
+
+#returns pace time in seconds  
 def countPace():
-    return int(float(parseTime(time)) / float(distance))
-    
+    return int(float(parseTime(time)) / distance)
+
 #returns formatted pace time
 def convertTime(ts):
     minutes = str(ts / 60)
@@ -37,7 +45,7 @@ def convertTime(ts):
     return minutes + ":" + seconds
 
 def summary():
-    print "Distance:", distance + "km"
+    print "Distance:", str(distance) + "km"
     print "Time:", time
     print "Pace:", convertTime(countPace())
 
