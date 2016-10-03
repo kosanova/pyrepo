fd27efa29cf9d75c8300995abfef04385ed4ad3e
emickos <michal.kosinski@ericsson.com>
2016-10-03 11:18:19 +0200

    Time parsing improved

--- pace_counter.py
+++ pace_counter.py
@@ -2,12 +2,12 @@
 
 # TODO list
 #
-# time as a tuple or sth?
 # implement option negativesplit
 # learn how to parse an xml file in the best way
 # parse several tcx files, think of chart framework to visualize the progress (all of them already downloaded into dropbox)
 # https://tapiriik.com/
 # refactor project, maybe OOP?
+# web presentation - Django!
 #
 # last and the hardest task - try to login into endomondo and download all tcx files OR just navigate through the site and store time and distance info
 
@@ -16,7 +16,7 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-d", "--distance", help = "The distance to run", type = float)
-parser.add_argument("time", help = "Goal time")
+parser.add_argument("time", help = "Goal time, possible formats: hh:mm:ss, mm:ss")
 parser.add_argument("-m", "--marathon", help = "The marathon distance", action = "store_true")
 parser.add_argument("-hm", "--halfmarathon", help = "The half-marathon distance", action = "store_true")
 args = parser.parse_args()
@@ -32,20 +32,16 @@ TIME = args.time
 
 # returns time in seconds
 def parse_time(str_time):
-    
-    delimiter_right_idx = str_time.rfind(':')
-    delimiter_left_idx = str_time.find(':')
 
-    if delimiter_right_idx != delimiter_left_idx:
-        hours = str_time[:delimiter_left_idx]
-        minutes = str_time[delimiter_left_idx+1:delimiter_right_idx]
+    t = str_time.split(':')
+    
+    if len(t) == 3:
+        return int(t[0]) * 60 * 60 + int(t[1]) * 60 + int(t[2])
+    elif len(t) == 2:
+        return int(t[0]) * 60 + int(t[1])
     else:
-        hours = 0
-        minutes = str_time[:delimiter_right_idx]
-
-    seconds = str_time[delimiter_right_idx+1:]
-    
-    return int(hours)*60*60 + int(minutes)*60 + int(seconds)
+        print "Wrong time format!\n"
+        return -1 
 
 # returns formatted pace time
 def convert_time(ts):
