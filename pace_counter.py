bd219d1c2314607286051518610fbde361d414f6
emickos <michal.kosinski@ericsson.com>
2016-10-04 17:12:06 +0200

    Negative split alogorithm - first shape

--- pace_counter.py
+++ pace_counter.py
@@ -2,16 +2,18 @@
 
 # TODO list
 #
-# implement option negativesplit
+# implement option negativesplit and visualize it on the chart
 # learn how to parse an xml file in the best way
-# parse several tcx files, think of chart framework to visualize the progress (all of them already downloaded into dropbox)
-# https://tapiriik.com/
 # refactor project, maybe OOP?
 # web presentation - Django!
 #
-# last and the hardest task - try to login into endomondo and download all tcx files OR just navigate through the site and store time and distance info
+# last and the hardest task - try to login into endomondo and download all tcx files
+# OR just navigate through the site and store time and distance info
 
 import argparse
+
+global DISTANCE
+global TIME
 
 parser = argparse.ArgumentParser()
 
@@ -19,7 +21,9 @@ parser.add_argument("-d", "--distance", help = "The distance to run", type = flo
 parser.add_argument("time", help = "Goal time, possible formats: hh:mm:ss, mm:ss")
 parser.add_argument("-m", "--marathon", help = "The marathon distance", action = "store_true")
 parser.add_argument("-hm", "--halfmarathon", help = "The half-marathon distance", action = "store_true")
+parser.add_argument("-n", "--negativesplit", help = "Print out the negative split strategy", action = "store_true")
 args = parser.parse_args()
+
 
 if args.marathon:
     DISTANCE = 42.195
@@ -27,6 +31,7 @@ elif args.halfmarathon:
     DISTANCE = 21.0975
 else:
     DISTANCE = args.distance
+
     
 TIME = args.time
 
@@ -48,18 +53,36 @@ def convert_time(ts):
     minutes = str(ts / 60)
     seconds = str(ts % 60)
     if int(seconds) < 10:
-        seconds = "0" + seconds
-    return minutes + ":" + seconds
+        seconds = '0' + seconds
+    return minutes + ':' + seconds
 
 # returns pace time in seconds  
 def count_pace():
     t = parse_time(TIME)
     return int(float(t) / DISTANCE)
 
+def negative_split():
+    # for testing, later as an argument
+    first_lap = 5 * 60
+    fake_time = first_lap * DISTANCE # 50
+    print "Fake time", fake_time
+    last_lap = (parse_time(TIME) - first_lap) / DISTANCE # (45 - 5) / 10 = 4
+    print "Last lap", last_lap
+    diff = (first_lap - last_lap) / (DISTANCE - 1) # 6.6667
+    print "Diff", diff
+    table = [first_lap]
+    for i in range (1,10):
+        first_lap -= diff
+        table.append(first_lap)
+
+    print table;
+
 def summary():
     print "Distance:", str(DISTANCE) + "km"
     print "Time:", TIME
     pace = count_pace()
     print "Pace:", convert_time(pace)
+    if args.negativesplit:
+        print "Pace strategy:", negative_split()
 
 summary()
